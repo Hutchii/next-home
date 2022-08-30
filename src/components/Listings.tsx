a9726906c06d8befc91d23f9100b9bcc33b754b3
Hutchii <ravodola@gmail.com>
2022-08-30 18:32:14 +0200

    Refactoring code and typesafety

--- src/components/Listings.tsx
+++ src/components/Listings.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { inferQueryOutput, trpc } from "../utils/trpc";
 import { Listbox } from "@headlessui/react";
-import { forOptions, sortOptions } from "../data/selectOptions";
-import { useController, UseControllerProps, useForm } from "react-hook-form";
+import { forOptions, Options, sortOptions } from "../data/selectOptions";
+import { Controller, useForm } from "react-hook-form";
 
 import Tag from "../../public/svg/tag.svg";
 import DropdownArrow from "../../public/svg/dropdownArrow.svg";
@@ -14,92 +14,37 @@ import Area from "../../public/svg/area.svg";
 import Clear from "../../public/svg/x.svg";
 import Image from "next/future/image";
 
-type Options = {
-  id: number;
-  name: string;
-};
-
-type OptionsSort = Options & { value: string; order: string };
-
 type SelectProps = {
   options: Options[];
   name: string;
   children: React.ReactElement;
+  multiple?: boolean;
+  valueIsObject?: boolean;
+  onChange: () => void;
+  value: string | { name: string; value: string; order: string };
 };
 
-type ForwardedSortValue = { name: string; value: string; order: string };
-
-const SelectMultiple = (props: SelectProps & UseControllerProps) => {
-  const {
-    field: { value, onChange, name },
-  } = useController(props);
-
-  const shownValues = value?.map((item: Options) => item.name).join(", ");
-
+const Select = (props: SelectProps) => {
   return (
     <Listbox
       as="div"
       className="relative w-full flex-[1_1_300px]"
-      name={name}
-      value={value}
-      onChange={onChange}
-      multiple
+      value={props.value}
+      onChange={props.onChange}
+      name={props.name}
+      multiple={props.multiple}
     >
       <Listbox.Button className="flex h-10 w-full items-center rounded-full border border-blue-300 bg-blue-100/20 pl-4 text-xs">
         {props.children}
         <span className="text-grey-500">{props.name}&nbsp;</span>
         <span className="pr-4 font-medium text-blue-800">
-          {shownValues ? shownValues : "Select an option"}
-        </span>
-        <DropdownArrow aria-hidden="true" className="ml-auto mr-4" />
-      </Listbox.Button>
-      <Listbox.Options className="absolute z-10 mt-1 w-full rounded-2xl border border-blue-300 bg-[#FBFCFF] text-xs">
-        {props.options.map((item) => (
-          <Listbox.Option
-            key={item.id}
-            className="relative cursor-pointer select-none py-2.5 pr-4 hover:rounded-2xl hover:bg-blue-100"
-            value={item.name}
-          >
-            {({ selected }) => (
-              <span
-                className={`block pl-11 ${
-                  selected ? "font-medium" : "font-normal"
-                }`}
-              >
-                {selected ? (
-                  <Check
-                    aria-hidden="true"
-                    className="absolute left-0 top-1/2 mr-2 ml-4 -translate-y-1/2"
-                  />
-                ) : null}
-                {item.name}
-              </span>
-            )}
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
-  );
-};
-
-const Select = (props: SelectProps & UseControllerProps) => {
-  const {
-    field: { value, onChange, name },
-  } = useController(props);
-
-  return (
-    <Listbox
-      as="div"
-      className="relative w-full flex-[1_1_300px]"
-      value={value}
-      onChange={onChange}
-      name={name}
-    >
-      <Listbox.Button className="flex h-10 w-full items-center rounded-full border border-blue-300 bg-blue-100/20 pl-4 text-xs">
-        {props.children}
-        <span className="text-grey-500">{props.name}&nbsp;</span>
-        <span className="pr-4 font-medium text-blue-800">
-          {value ? value.name : "Select an option"}
+          {props.valueIsObject
+            ? props.value.name
+            : !props.value || props.value.length === 0
+            ? "Select an option"
+            : props.multiple
+            ? props.value?.map((item: Options) => item).join(", ")
+            : props.value}
         </span>
         <DropdownArrow aria-hidden="true" className="ml-auto mr-4" />
       </Listbox.Button>
@@ -108,79 +53,28 @@ const Select = (props: SelectProps & UseControllerProps) => {
           <Listbox.Option
             key={item.id}
             className="relative cursor-pointer select-none py-2.5 pr-4 hover:rounded-2xl  hover:bg-blue-100"
-            value={item.name}
+            value={!props.valueIsObject ? item.name : item}
           >
-            <span
-              className={`block pl-11 ${
-                item.name === (value as Options)?.name
-                  ? "font-medium"
-                  : "font-normal"
-              }`}
-            >
-              {item.name === (value as Options)?.name ? (
-                <Check
-                  aria-hidden="true"
-                  className="absolute left-0 top-1/2 mr-2 ml-4 -translate-y-1/2"
-                />
-              ) : null}
-              {item.name}
-            </span>
-          </Listbox.Option>
-        ))}
-      </Listbox.Options>
-    </Listbox>
-  );
-};
-
-const SelectSort = ({
-  name,
-  value,
-  onChange,
-  options,
-  children,
-}: {
-  name: string;
-  value: ForwardedSortValue;
-  onChange: (v: ForwardedSortValue) => void;
-  options: OptionsSort[];
-  children: React.ReactElement;
-}) => {
-  return (
-    <Listbox
-      as="div"
-      className="relative w-full md:w-80"
-      value={value}
-      onChange={onChange}
-      name={name}
-    >
-      <Listbox.Button className="flex h-10 w-full items-center rounded-full border border-blue-300 bg-blue-100/20 pl-4 text-xs">
-        {children}
-        <span className="text-grey-500">{name}&nbsp;</span>
-        <span className="pr-4 font-medium text-blue-800">
-          {value ? value.name : "Select an option"}
-        </span>
-        <DropdownArrow aria-hidden="true" className="ml-auto mr-4" />
-      </Listbox.Button>
-      <Listbox.Options className="absolute z-10 mt-1 w-full rounded-2xl border border-blue-300 bg-[#FBFCFF] text-xs">
-        {options.map((item) => (
-          <Listbox.Option
-            key={item.id}
-            className="relative cursor-pointer select-none py-2.5 pr-4 hover:rounded-2xl  hover:bg-blue-100"
-            value={{ name: item.name, value: item.value, order: item.order }}
-          >
-            <span
-              className={`block pl-11 ${
-                item.name === value.name ? "font-medium" : "font-normal"
-              }`}
-            >
-              {item.name === value.name ? (
-                <Check
-                  aria-hidden="true"
-                  className="absolute left-0 top-1/2 mr-2 ml-4 -translate-y-1/2"
-                />
-              ) : null}
-              {item.name}
-            </span>
+            {({ selected }) => {
+              const selectedCondition = props.valueIsObject
+                ? item.name === props.value.name
+                : selected;
+              return (
+                <span
+                  className={`block pl-11 ${
+                    selectedCondition ? "font-medium" : "font-normal"
+                  }`}
+                >
+                  {selectedCondition ? (
+                    <Check
+                      aria-hidden="true"
+                      className="absolute left-0 top-1/2 mr-2 ml-4 -translate-y-1/2"
+                    />
+                  ) : null}
+                  {item.name}
+                </span>
+              );
+            }}
           </Listbox.Option>
         ))}
       </Listbox.Options>
@@ -259,7 +153,7 @@ const Items = ({
                 <div className="flex gap-5 pt-2.5">
                   <div className="flex items-center gap-1">
                     <Area className="mt-[1px]" />
-                    <p className="text-xs text-grey-500">{item.area}</p>
+                    <p className="text-xs text-grey-500">{item.area} m²</p>
                   </div>
                   <div className="flex items-center gap-1">
                     <Area className="mt-[1px]" />
@@ -317,9 +211,7 @@ const Listings = () => {
       ...data,
       Sort: { name: "Largest Price", value: "price", order: "desc" },
     });
-    console.log(data);
   });
-  console.log(formData, "FORM");
   return (
     <>
       <section className="mx-auto -mt-20 sm:px-6 lg:-mt-10 lg:px-10 xl:w-4/5 xl:px-0 4xl:w-[65vw] 4xl:max-w-[1530px]">
@@ -331,30 +223,52 @@ const Listings = () => {
             Search by applying filters:
           </p>
           <div className="space-y-2.5 sm:flex sm:flex-wrap sm:gap-2.5 sm:space-y-0">
-            <SelectMultiple
-              options={forOptions}
+            <Controller
+              defaultValue={[]}
               name="For"
               control={control}
-              defaultValue={[]}
-            >
-              <Tag className="mr-2" aria-hidden="true" />
-            </SelectMultiple>
-            <Select
-              options={forOptions}
-              control={control}
+              render={({ field: { onChange, value, name } }) => (
+                <Select
+                  options={forOptions}
+                  name={name}
+                  onChange={onChange}
+                  value={value}
+                  multiple={true}
+                >
+                  <Tag className="mr-2" aria-hidden="true" />
+                </Select>
+              )}
+            />
+            <Controller
+              defaultValue=""
               name="Type"
-              defaultValue=""
-            >
-              <Home className="mr-2" aria-hidden="true" />
-            </Select>
-            <Select
-              options={forOptions}
               control={control}
-              name="City"
+              render={({ field: { onChange, value, name } }) => (
+                <Select
+                  options={forOptions}
+                  name={name}
+                  onChange={onChange}
+                  value={value}
+                >
+                  <Home className="mr-2" aria-hidden="true" />
+                </Select>
+              )}
+            />
+            <Controller
               defaultValue=""
-            >
-              <Location className="mr-2" aria-hidden="true" />
-            </Select>
+              name="City"
+              control={control}
+              render={({ field: { onChange, value, name } }) => (
+                <Select
+                  options={forOptions}
+                  name={name}
+                  onChange={onChange}
+                  value={value}
+                >
+                  <Location className="mr-2" aria-hidden="true" />
+                </Select>
+              )}
+            />
           </div>
           <div className="space-y-2.5 sm:flex sm:flex-wrap sm:gap-2.5 sm:space-y-0">
             <div className="flex w-full flex-[1_1_320px] items-center gap-1">
@@ -401,11 +315,12 @@ const Listings = () => {
         </form>
       </section>
       <Items data={data} isLoading={isLoading}>
-        <SelectSort
+        <Select
           options={sortOptions}
           name="Sort"
+          valueIsObject
           value={formData.Sort}
-          onChange={(v: ForwardedSortValue) =>
+          onChange={(v: any) =>
             setFormData({
               ...formData,
               Sort: { ...v, value: v.value, order: v.order },
@@ -413,7 +328,7 @@ const Listings = () => {
           }
         >
           <Home className="mr-2" aria-hidden="true" />
-        </SelectSort>
+        </Select>
       </Items>
     </>
   );
--- src/data/selectOptions.ts
+++ src/data/selectOptions.ts
@@ -1,9 +1,16 @@
-export const forOptions = [
+export type Options = {
+  id: number;
+  name: string;
+};
+
+export type OptionsSort = Options & { value: string; order: string };
+
+export const forOptions: Options[] = [
   { id: 1, name: "Apartments" },
   { id: 2, name: "Houses" },
 ];
 
-export const sortOptions = [
+export const sortOptions: OptionsSort[] = [
   { id: 1, name: "Largest Price", value: "price", order: "desc" },
   { id: 2, name: "Lowest Price", value: "price", order: "asc" },
 ];
