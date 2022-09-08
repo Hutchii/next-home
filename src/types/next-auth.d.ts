94429255dc84a015268710b38b1d068d30938b98
Hutchii <ravodola@gmail.com>
2022-09-08 19:15:28 +0200

    Added types for next-auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,9 +4,17 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
+  enum Role {
+    USER = "USER",
+    PREMIUM = "PREMIUM",
+  }
   interface Session {
     user?: {
-      id: string;
+      id?: string;
+      role?: Role;
     } & DefaultSession["user"];
+  }
+  interface User {
+    role?: Role,
   }
 }
