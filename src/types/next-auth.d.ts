b6cc2653580b19226f5816599c63cbc60a10f03d
Hutchii <ravodola@gmail.com>
2022-09-15 00:10:30 +0200

    Fixed types in nextAuth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,17 +4,9 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  enum Role {
-    USER = "USER",
-    PREMIUM = "PREMIUM",
-  }
   interface Session {
     user?: {
-      id?: string;
-      role?: Role;
+      id: string;
     } & DefaultSession["user"];
-  }
-  interface User {
-    role?: Role,
   }
 }
